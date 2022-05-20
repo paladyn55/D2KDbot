8d6c804d6fdfa1380d06cdfe00d02fccdbb6dbae
Joe Cassidy <69397252+paladyn55@users.noreply.github.com>
2022-05-20 11:39:22 +0000

    sorting response paths

--- api_get.py
+++ api_get.py
@@ -1,16 +1,22 @@
 import requests
 import os
+import pprint
 key = os.environ['BNG_KEY']
 # dictionary to hold extra headers
 HEADERS = {"X-API-Key": key}
 
 def stat_get(ID, platform):
+	usr = requests.get("https://bungie.net/platform/" + f"/Destiny2/{platform}/Account/{ID}/Character/0/Stats/", headers=HEADERS)
+	usr = usr.json()
+	pprint.pprint(dir(usr.values))
+	
 	r = requests.get("https://www.bungie.net/platform/Destiny2/" + platform + "/Account/" + ID + "/Stats/?mode=84", headers=HEADERS)
 	response = r.json()
 	characterStats = response["Response"]["mergedAllCharacters"]
 	#activity numbers: trials - 84, comp - 69 (eyyy), valor - 70, alllllll pvp - 5 (only seasonal stat used)
-	for stat in response['Response']['mergedAllCharacters']['results']:
-		print(stat)
+	#for stat in response['Response']['mergedAllCharacters']['results']:
+	#	print(stat)
 	lifetimeKD = characterStats["results"]["allPvP"]["allTime"]["killsDeathsRatio"]["basic"]["value"]
 	#print(characterStats["results"]["trials"].keys())
 	print("Lifetime kd:", lifetimeKD)
+	
