e6d2797c2dc1e0650d58a795fbd611794a1c38c9
Joe Cassidy <69397252+paladyn55@users.noreply.github.com>
2022-05-20 08:05:58 +0000

    sorting through endpoints

--- api_get.py
+++ api_get.py
@@ -1,16 +1,16 @@
 import requests
-
+import os
+key = os.environ['BNG_KEY']
 # dictionary to hold extra headers
-HEADERS = {"X-API-Key": "898ee44d41714955909f5b4929cfd45a"}
+HEADERS = {"X-API-Key": key}
 
 def stat_get(ID, platform):
-	r = requests.get("https://www.bungie.net/platform/Destiny2/" + platform + "/Account/" + ID + "/Stats/?modes=5", headers=HEADERS)
+	r = requests.get("https://www.bungie.net/platform/Destiny2/" + platform + "/Account/" + ID + "/Stats/?mode=84", headers=HEADERS)
 	response = r.json()
 	characterStats = response["Response"]["mergedAllCharacters"]
 	#activity numbers: trials - 84, comp - 69 (eyyy), valor - 70, alllllll pvp - 5 (only seasonal stat used)
-
-
-	
+	for stat in response['Response']['mergedAllCharacters']['results']:
+		print(stat)
 	lifetimeKD = characterStats["results"]["allPvP"]["allTime"]["killsDeathsRatio"]["basic"]["value"]
 	#print(characterStats["results"]["trials"].keys())
 	print("Lifetime kd:", lifetimeKD)
