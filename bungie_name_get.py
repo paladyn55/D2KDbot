863f32ccc0ccfde1e4c22701baf192537565e100
Joe Cassidy <69397252+paladyn55@users.noreply.github.com>
2022-05-23 09:59:00 +0000

    added new txt file with bungie names and discord names

--- bungie_name_get.py
+++ bungie_name_get.py
@@ -2,26 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 options = Options()
-options.headless = True
+#options.headless = True
 import os
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), service_log_path=os.devnull, options=options)
 from selenium.webdriver.common.by import By
 def run():
 	name_xp = '/html/body/div[1]/div[2]/div[2]/div/main/div[2]/div[1]/div[2]/div[2]/div[2]/span/span[1]'
 	num_xp = '/html/body/div[1]/div[2]/div[2]/div/main/div[2]/div[1]/div[2]/div[2]/div[2]/span/span[2]'
-	names = (open('DiscName.txt', 'r')).readlines
-	nums = (open('D2Tpage.txt', 'r')).readlines
-	f = open('new.txt', 'w')
+	names = (open('DiscName.txt', 'r')).readlines()
+	nums = (open('D2Tpage.txt', 'r')).readlines()
+	
 	usr_arr = []
-	for i in len(names):
-		usr_arr.append(names[i])
-		usr_arr.append(nums[i])
+	for i in range(len(names)):
+		temp = []
+		temp.append(names[i].strip('\n'))
+		temp.append(nums[i].strip('\n'))
+		usr_arr.append(temp)
+		print(temp)
+	print(usr_arr)
 	for usr in usr_arr:
 		url_DTR = "https://destinytracker.com/destiny-2/profile/bungie/" + usr[1] + "/overview"
 		driver.get(url_DTR)
-		b_name = driver.find_element(By.XPATH, name_xp)
-		b_num = driver.find_element(By.XPATH, num_xp)
+		b_name = (driver.find_element(By.XPATH, name_xp).text)
+		b_num = (driver.find_element(By.XPATH, num_xp).text)
 		usr.append(b_name)
 		usr.append(b_num)
-	f.writelines(usr_arr)
-		
+		temp =str(f'{str(usr[0])} {str(usr[1])} {str(usr[2])} {str(usr[3])}\n')
+		print(temp)
+		f = open('new.txt', 'a')
+		f.write(str(temp))
+		f.close()
+	print(usr_arr)
+	
